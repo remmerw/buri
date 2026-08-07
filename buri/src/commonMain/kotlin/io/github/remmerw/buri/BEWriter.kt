0743e43cd5cf1866966d1b420bc1ec5db09b4f00
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-07 07:54:20 +0545

    Aktualisieren von BEWriter.kt

--- buri/src/commonMain/kotlin/io/github/remmerw/buri/BEWriter.kt
+++ buri/src/commonMain/kotlin/io/github/remmerw/buri/BEWriter.kt
@@ -1,44 +1,7 @@
 package io.github.remmerw.buri
 
-
 import kotlinx.io.Sink
 
-class BEWriter(val sink: Sink){
-    fun eof(){
-        sink.writeByte(EOF.code.toByte())
-    }
-   
-    fun map(){
-        sink.writeByte(MAP_PREFIX.code.toByte())
-    }
-
-    fun mapEntry(key: String){
-       val keyBytes = key.encodeToByteArray()
-      
-       // Write key length and delimiter
-        sink.write(keyBytes.size.toString().encodeToByteArray())
-      sink.writeByte(DELIMITER.code.toByte())
-     
-      sink.write(keyBytes)
-    }
-   
-    fun list(){
-        sink.writeByte(LIST_PREFIX.code.toByte())
-    }
-    
-    fun longValue(value: Long){
-       sink.writeByte(INTEGER_PREFIX.code.toByte())
-   sink.write(value.toString().encodeToByteArray())
-       eof()
-    }
-
-    fun dataValue(content: ByteArray){
-               sink.write(content.size.toString().encodeToByteArray())
-        sink.writeByte(DELIMITER.code.toByte())
-        sink.write(content)
-    }
- 
-}
 
 fun Sink.bencode(content: ByteArray){
  this.write(content.size.toString().encodeToByteArray())
@@ -66,7 +29,7 @@ fun Sink.bencodeList(){
    this.writeByte(LIST_PREFIX.code.toByte())
 }
 
-fun Sink.bencodeMapEntry(key: String){
+fun Sink.bencodeMapKey(key: String){
    val keyBytes = key.encodeToByteArray()
       
    // Write key length and delimiter
