f5cbafef8961d664bd765e13a01a5b4e0d73f9d2
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-11 18:08:22 +0545

    Aktualisieren von Buffer.kt

--- buri/src/commonMain/kotlin/io/github/remmerw/buri/Buffer.kt
+++ buri/src/commonMain/kotlin/io/github/remmerw/buri/Buffer.kt
@@ -1,20 +1,19 @@
 class Buffer(private val data: ByteArray) {
-    // Zweiter Konstruktor für eine bequemere Instanziierung über die Größe
+    
     constructor(size: Int) : this(ByteArray(size))
 
-    // Interner Schreib- und Lese-Index
     private var index: Int = 0
 
     // Kotlin-Property statt Funktion length()
     val length: Int
         get() = index
 
-    fun write(byte: Byte) {
+    fun writeBuffer(byte: Byte) {
         if (index < data.size) {
             data[index] = byte
             index++
         } else {
-            throw IndexOutOfBoundsException("Buffer ist voll. Index: $index, Kapazität: ${data.size}")
+            throw Exception()
         }
     }
 
@@ -23,7 +22,7 @@ class Buffer(private val data: ByteArray) {
             bytes.copyInto(destination = data, destinationOffset = index)
             index += bytes.size
         } else {
-            throw IndexOutOfBoundsException("Nicht genug Platz im Buffer. Benötigt: ${bytes.size}, Verfügbar: $remaining")
+            throw Exception()
         }
     }
 
