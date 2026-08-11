98c85410075567d317266d8dafcdcc331d023e98
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-11 18:06:08 +0545

    Aktualisieren von BEWriter.kt

--- buri/src/commonMain/kotlin/io/github/remmerw/buri/BEWriter.kt
+++ buri/src/commonMain/kotlin/io/github/remmerw/buri/BEWriter.kt
@@ -47,3 +47,52 @@ fun Sink.bencodeMapKey(key: String) {
     this.writeByte(DELIMITER.code.toByte())
     this.write(keyBytes)
 }
+
+
+
+
+fun Buffer.bencode(value: ByteArray) {
+    this.write(value.size.toString().encodeToByteArray())
+    this.writeByte(DELIMITER.code.toByte())
+    this.write(value)
+}
+
+fun Buffer.bencode(value: BEObject) {
+    value.encodeTo(this)
+}
+
+fun Buffer.bencode(value: String) {
+    this.bencode(value.encodeToByteArray())
+}
+
+fun Buffer.bencode(value: Long) {
+    this.writeByte(INTEGER_PREFIX.code.toByte())
+    this.write(value.toString().encodeToByteArray())
+    this.writeByte(EOF.code.toByte())
+}
+
+fun Buffet.bencode(value: Int) {
+    this.bencode(value.toLong())
+}
+
+fun Buffer.bencodeEof() {
+    this.writeByte(EOF.code.toByte())
+}
+
+fun Buffer.bencodeMap() {
+    this.writeByte(MAP_PREFIX.code.toByte())
+}
+
+fun Buffer.bencodeList() {
+    this.writeByte(LIST_PREFIX.code.toByte())
+}
+
+fun Buffer.bencodeMapKey(key: String) {
+    val keyBytes = key.encodeToByteArray()
+
+    // Write key length and delimiter
+    this.write(keyBytes.size.toString().encodeToByteArray())
+
+    this.writeByte(DELIMITER.code.toByte())
+    this.write(keyBytes)
+}
