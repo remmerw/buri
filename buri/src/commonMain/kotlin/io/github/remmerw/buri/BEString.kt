092cb74ee725ddead9d711d5815174a4a2a93d1f
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-11 18:14:45 +0545

    Aktualisieren von BEString.kt

--- buri/src/commonMain/kotlin/io/github/remmerw/buri/BEString.kt
+++ buri/src/commonMain/kotlin/io/github/remmerw/buri/BEString.kt
@@ -10,7 +10,9 @@ value class BEString(
     override fun encodeTo(sink: Sink) {
         sink.bencode(content)
     }
-
+    override fun encodeTo(buffer: Buffer) {
+        buffer.bencode(content)
+    }
     override fun toString(): String = content.decodeToString()
 
     fun toByteArray(): ByteArray = content
