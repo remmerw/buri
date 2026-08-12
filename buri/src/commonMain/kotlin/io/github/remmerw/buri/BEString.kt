82673d4f50d7f0a226ca2b3abd703cbd1b74f967
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-12 20:31:31 +0545

    Aktualisieren von BEString.kt

--- buri/src/commonMain/kotlin/io/github/remmerw/buri/BEString.kt
+++ buri/src/commonMain/kotlin/io/github/remmerw/buri/BEString.kt
@@ -1,15 +1,11 @@
 package io.github.remmerw.buri
 
-import kotlinx.io.Sink
 import kotlin.jvm.JvmInline
 
 @JvmInline
 value class BEString(
     private val content: ByteArray,
 ) : BEObject {
-    override fun encodeTo(sink: Sink) {
-        sink.bencode(content)
-    }
 
     override fun encodeTo(buffer: Buffer) {
         buffer.bencode(content)
