3312078d36c991fbe349411e91620661e4aaccc0
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-12 20:30:14 +0545

    Aktualisieren von BEList.kt

--- buri/src/commonMain/kotlin/io/github/remmerw/buri/BEList.kt
+++ buri/src/commonMain/kotlin/io/github/remmerw/buri/BEList.kt
@@ -1,21 +1,11 @@
 package io.github.remmerw.buri
 
-import kotlinx.io.Sink
 import kotlin.jvm.JvmInline
 
 @JvmInline
 value class BEList(
     private val list: List<BEObject>,
 ) : BEObject {
-    override fun encodeTo(sink: Sink) {
-        sink.bencodeList()
-
-        list.forEach { value ->
-            value.encodeTo(sink)
-        }
-
-        sink.bencodeEof()
-    }
 
     override fun encodeTo(buffer: Buffer) {
         buffer.bencodeList()
