a46586e7ae0b9c4fffd95317971e86b5d313b960
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-12 20:21:02 +0545

    Aktualisieren von Tests.kt

--- buri/src/commonTest/kotlin/io/github/remmerw/buri/Tests.kt
+++ buri/src/commonTest/kotlin/io/github/remmerw/buri/Tests.kt
@@ -1,6 +1,5 @@
 package io.github.remmerw.buri
 
-import kotlinx.io.Buffer
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
@@ -10,7 +9,7 @@ class Tests {
     @Test
     fun encodeDecodeString() {
         val testData = "hi"
-        val buffer = Buffer()
+        val buffer = Buffer(100)
         testData.bencode().encodeTo(buffer)
         val cmp = decodeBencodeToString(buffer)
         assertEquals(cmp, testData)
@@ -19,7 +18,7 @@ class Tests {
     @Test
     fun encodeDecodeInteger() {
         val value = 6666L
-        val buffer = Buffer()
+        val buffer = Buffer(100)
         value.bencode().encodeTo(buffer)
         val cmp = decodeBencodeToLong(buffer)
         assertEquals(cmp, value)
@@ -28,7 +27,7 @@ class Tests {
     @Test
     fun encodeDecodeEmptyList() {
         val value: List<BEObject> = emptyList()
-        val buffer = Buffer()
+        val buffer = Buffer(100)
         value.encodeBencodeTo(buffer)
         val cmp = decodeBencodeToList(buffer)
         assertEquals(cmp, value)
@@ -37,7 +36,7 @@ class Tests {
     @Test
     fun encodeDecodeEmptyMap() {
         val value: Map<String, BEObject> = emptyMap()
-        val buffer = Buffer()
+        val buffer = Buffer(100)
         value.encodeBencodeTo(buffer)
         val cmp = decodeBencodeToMap(buffer)
         assertEquals(cmp, value)
@@ -50,7 +49,7 @@ class Tests {
                 555L.bencode(),
                 "hello".bencode(),
             )
-        val buffer = Buffer()
+        val buffer = Buffer(200)
         value.encodeBencodeTo(buffer)
 
         val list = decodeBencodeToList(buffer)
@@ -78,7 +77,7 @@ class Tests {
                 555L.bencode(),
                 "hello".bencode(),
             )
-        val buffer = Buffer()
+        val buffer = Buffer(200)
 
         // encode
         value.encodeBencodeTo(buffer)
