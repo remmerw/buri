c43692611bb068832abde0dc3b495e3a2cdb1e0b
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-12 20:30:56 +0545

    Aktualisieren von BEMap.kt

--- buri/src/commonMain/kotlin/io/github/remmerw/buri/BEMap.kt
+++ buri/src/commonMain/kotlin/io/github/remmerw/buri/BEMap.kt
@@ -1,23 +1,11 @@
 package io.github.remmerw.buri
 
-import kotlinx.io.Sink
 import kotlin.jvm.JvmInline
 
 @JvmInline
 value class BEMap(
     private val map: Map<String, BEObject>,
 ) : BEObject {
-    override fun encodeTo(sink: Sink) {
-        sink.bencodeMap()
-
-        map.entries
-            .sortedBy { it.key }
-            .forEach { (key, value) ->
-                sink.bencodeMapKey(key)
-                value.encodeTo(sink)
-            }
-        sink.bencodeEof()
-    }
 
     override fun encodeTo(buffer: Buffer) {
         buffer.bencodeMap()
