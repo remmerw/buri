6939fca7bcbdee5a172a6b3aa2753c9f7feea9b9
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-13 18:03:16 +0545

    Aktualisieren von BEWriter.kt

--- buri/src/commonMain/kotlin/io/github/remmerw/buri/BEWriter.kt
+++ buri/src/commonMain/kotlin/io/github/remmerw/buri/BEWriter.kt
@@ -1,5 +1,7 @@
 package io.github.remmerw.buri
 
+import kotlinx.io.Sink
+
 fun Buffer.bencodeArray(size: Int) {
     this.write(size.toString().encodeToByteArray())
     this.writeByte(DELIMITER.code.toByte())
@@ -50,6 +52,71 @@ fun Buffer.bencodeList() {
 }
 
 fun Buffer.bencodeMapKey(key: String) {
+    val keyBytes = key.encodeToByteArray()
+
+    // Write key length and delimiter
+    this.write(keyBytes.size.toString().encodeToByteArray())
+
+    this.writeByte(DELIMITER.code.toByte())
+    this.write(keyBytes)
+}
+
+
+
+
+
+
+
+fun Sink.bencodeArray(size: Int) {
+    this.write(size.toString().encodeToByteArray())
+    this.writeByte(DELIMITER.code.toByte())
+}
+
+fun Sink.bencodeArrayData(value: ByteArray) {
+    this.write(value)
+}
+
+fun Sink.bencodeArrayData(value: UShort) {
+    this.writeByte((value.toInt() shr 8).toByte())
+    this.writeByte((value.toInt() and 0xFF).toByte())
+}
+
+fun Sink.bencodeArrayData(value: Byte) {
+    this.writeByte(value)
+}
+
+fun Sunk.bencode(value: ByteArray) {
+    this.bencodeArray(value.size)
+    this.bencodeArrayData(value)
+}
+
+fun Sink.bencode(value: String) {
+    this.bencode(value.encodeToByteArray())
+}
+
+fun Sink.bencode(value: Long) {
+    this.writeByte(INTEGER_PREFIX.code.toByte())
+    this.write(value.toString().encodeToByteArray())
+    this.writeByte(EOF.code.toByte())
+}
+
+fun Sink.bencode(value: Int) {
+    this.bencode(value.toLong())
+}
+
+fun Sink.bencodeEof() {
+    this.writeByte(EOF.code.toByte())
+}
+
+fun Sink.bencodeMap() {
+    this.writeByte(MAP_PREFIX.code.toByte())
+}
+
+fun Sink.bencodeList() {
+    this.writeByte(LIST_PREFIX.code.toByte())
+}
+
+fun Sink.bencodeMapKey(key: String) {
     val keyBytes = key.encodeToByteArray()
 
     // Write key length and delimiter
