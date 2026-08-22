35012d4c0e9a7f6c432f0808a10507d2dac8aa20
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-22 15:37:13 +0545

    Aktualisieren von BEObject.kt

--- buri/src/commonMain/kotlin/io/github/remmerw/buri/BEObject.kt
+++ buri/src/commonMain/kotlin/io/github/remmerw/buri/BEObject.kt
@@ -1,9 +1,10 @@
 package io.github.remmerw.buri
 
 import kotlinx.io.Sink
+import java.nio.ByteBuffer
 
 interface BEObject {
-    fun encodeTo(buffer: Buffer)
+    fun encodeTo(buffer: ByteBuffer)
 
     fun encodeTo(sink: Sink)
 }
