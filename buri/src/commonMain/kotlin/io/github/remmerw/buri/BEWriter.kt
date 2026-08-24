5a8fc5a4cf0273f2b6f7228804e94180f2ceb2cf
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-24 15:27:06 +0545

    Aktualisieren von BEWriter.kt

--- buri/src/commonMain/kotlin/io/github/remmerw/buri/BEWriter.kt
+++ buri/src/commonMain/kotlin/io/github/remmerw/buri/BEWriter.kt
@@ -1,6 +1,6 @@
 package io.github.remmerw.buri
 
-import kotlinx.io.Sink
+
 import java.nio.ByteBuffer
 
 fun ByteBuffer.bencodeArray(size: Int) {
@@ -62,61 +62,3 @@ fun ByteBuffer.bencodeMapKey(key: String) {
     this.put(keyBytes)
 }
 
-fun Sink.bencodeArray(size: Int) {
-    this.write(size.toString().encodeToByteArray())
-    this.writeByte(DELIMITER.code.toByte())
-}
-
-fun Sink.bencodeArrayData(value: ByteArray) {
-    this.write(value)
-}
-
-fun Sink.bencodeArrayData(value: UShort) {
-    this.writeByte((value.toInt() shr 8).toByte())
-    this.writeByte((value.toInt() and 0xFF).toByte())
-}
-
-fun Sink.bencodeArrayData(value: Byte) {
-    this.writeByte(value)
-}
-
-fun Sink.bencode(value: ByteArray) {
-    this.bencodeArray(value.size)
-    this.bencodeArrayData(value)
-}
-
-fun Sink.bencode(value: String) {
-    this.bencode(value.encodeToByteArray())
-}
-
-fun Sink.bencode(value: Long) {
-    this.writeByte(INTEGER_PREFIX.code.toByte())
-    this.write(value.toString().encodeToByteArray())
-    this.writeByte(EOF.code.toByte())
-}
-
-fun Sink.bencode(value: Int) {
-    this.bencode(value.toLong())
-}
-
-fun Sink.bencodeEof() {
-    this.writeByte(EOF.code.toByte())
-}
-
-fun Sink.bencodeMap() {
-    this.writeByte(MAP_PREFIX.code.toByte())
-}
-
-fun Sink.bencodeList() {
-    this.writeByte(LIST_PREFIX.code.toByte())
-}
-
-fun Sink.bencodeMapKey(key: String) {
-    val keyBytes = key.encodeToByteArray()
-
-    // Write key length and delimiter
-    this.write(keyBytes.size.toString().encodeToByteArray())
-
-    this.writeByte(DELIMITER.code.toByte())
-    this.write(keyBytes)
-}
